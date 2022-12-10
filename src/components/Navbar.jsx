d0bd866a3075318f3375d07c78e2d4c83e5564ea
Ramil <zarram89@gmail.com>
2022-12-10 17:23:59 +0500

    Adds final styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import logo from "../img/react-logo.svg"
+// import logo from "../img/react-logo.svg {logo} "
 
 function Navbar() {
   return (
     <nav className="container__nav nav">
       <div className="nav__logo nav-logo">
-        <img className="logo__img logo-img" src={logo} width="30" height="30" alt="React logo" />
+        <img className="logo__img logo-img" src="../public/img/react-logo.svg" width="30" height="30" alt="React logo" />
         <h3 className="logo__text logo-text">ReactFacts</h3>
       </div>
       <h4 className="nav__theme nav-theme">React Course - Project 1</h4>
