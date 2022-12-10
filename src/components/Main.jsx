b1f5358a0501d168b2cccb9ffb128c3351ef3bea
Ramil <zarram89@gmail.com>
2022-12-10 15:10:46 +0500

    Init the repo

---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+function Main() {
+  return (
+    <h1>Main goes here</h1>
+  )
+}
+
+export default Main
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+function Navbar() {
+  return (
+    <h1>Navbar goes here</h1>
+  )
+}
+
+export default Navbar
