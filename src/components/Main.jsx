5dde81fba615602fa30d9812c2766737b6aeaf5c
Ramil <zarram89@gmail.com>
2022-12-10 16:54:49 +0500

    Adds styles to main content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,15 @@
 function Main() {
   return (
-    <h1>Main goes here</h1>
+    <main className="main">
+      <h1 className="main-title">Fun facts about React</h1>
+      <ul className="main-list">
+        <li className="list-item">Was first released in 2013</li>
+        <li className="list-item">Was originally created by Jordan Walke</li>
+        <li className="list-item">Has well over 100K stars on GitHub</li>
+        <li className="list-item">Is maintained by Facebook</li>
+        <li className="list-item">Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
   )
 }
 
