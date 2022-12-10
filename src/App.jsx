53ed5f16a74b47e9b85b4662a368615e94a96e72
Ramil <zarram89@gmail.com>
2022-12-10 16:35:09 +0500

    Adds styles for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Main from "./components/Main"
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Navbar />
       <Main />
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
+import logo from "../img/react-logo.svg"
+
 function Navbar() {
   return (
-    <h1>Navbar goes here</h1>
+    <nav className="container__nav nav">
+      <div className="nav__logo nav-logo">
+        <img className="logo__img logo-img" src={logo} width="30" height="30" alt="React logo" />
+        <h3 className="logo__text logo-text">ReactFacts</h3>
+      </div>
+      <h4 className="nav__theme nav-theme">React Course - Project 1</h4>
+    </nav>
   )
 }
 
